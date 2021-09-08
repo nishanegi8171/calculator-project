33920bd36d8305378cca615fb968876e8ca2f19b
Nisha <nishanegi.8171@gmail.com>
2021-09-09 02:13:44 +0530

    modify background

--- first.js
+++ first.js
@@ -1,16 +1,11 @@
 let val= document.getElementById('val');
 buttons= document.querySelectorAll('button');
 let sValue='';
-let p="undefined";
 for(item of buttons){
     item.addEventListener('click', (e)=>{
         buttonText=e.target.innerText;
-        console.log(buttonText);
-        if(buttonText=='*'){
-            sValue +=buttonText;
-            val.value = sValue;
-        }
-        else if(buttonText=='AC'){
+        
+        if(buttonText=='AC'){
             sValue=" ";
             val.value=sValue;
         }
